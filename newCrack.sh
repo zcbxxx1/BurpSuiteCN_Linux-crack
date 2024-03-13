4c10c237157d9722e3ce0d8044570f3d618ef69a
zcbxxx1 <1393281966@qq.com>
2024-03-13 21:43:08 +0800

    修正逻辑2

--- newCrack.sh
+++ newCrack.sh
@@ -43,91 +43,93 @@ while true; do
     fi
 done
 
-
 PS3='输入您的选择: '
 foods=("官方下载" "官方安装" "准备破解补丁" "运行注册机" "设置英文版" "设置汉化版" "退出安装..")
-select fav in "${foods[@]}"; do
-    case $fav in
-    "官方下载")
-        echo "$fav ...."
-        Version=$(curl -L -s -H 'Accept: application/json' "https://portswigger.net/burp/releases/data?previousLastId=-1&lastId=-1&pageSize=3" | sed -n 's/.*"ProductPlatformLabel":"Linux (x64)".*"Version":"\([^"]*\)".*/\1/p')
-        echo "当前最新版本为 $Version，正在尝试下载"
-        Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=$Version&type=Linux"
-        # echo ${Link}
-        Version=${Version//./_}
-        BpName="burpsuite_pro_linux_v$Version.sh"
-        curl "$Link" -o "$BpName"
-        echo "下载完成，文件名为 $BpName"
-        sleep 2
-        ;;
-    "官方安装")
-        echo "$fav ...."
-        if [ -f "$BpName" ]; then
-            echo "检测到安装包，是否立即安装？[Y/n]"
-            read -r INSTALL_CHOICE
-            if [ "$INSTALL_CHOICE" = "n" ] || [ "$INSTALL_CHOICE" = "N" ]; then
-                echo "取消安装！"
+while true; do
+    select fav in "${foods[@]}"; do
+        case $fav in
+        "官方下载")
+            echo "$fav ...."
+            Version=$(curl -L -s -H 'Accept: application/json' "https://portswigger.net/burp/releases/data?previousLastId=-1&lastId=-1&pageSize=3" | sed -n 's/.*"ProductPlatformLabel":"Linux (x64)".*"Version":"\([^"]*\)".*/\1/p')
+            echo "当前最新版本为 $Version，正在尝试下载"
+            Link="https://portswigger-cdn.net/burp/releases/download?product=pro&version=$Version&type=Linux"
+            # echo ${Link}
+            Version=${Version//./_}
+            BpName="burpsuite_pro_linux_v$Version.sh"
+            curl "$Link" -o "$BpName"
+            echo "下载完成，文件名为 $BpName"
+            sleep 2
+            ;;
+        "官方安装")
+            echo "$fav ...."
+            if [ -f "$BpName" ]; then
+                echo "检测到安装包，是否立即安装？[Y/n]"
+                read -r INSTALL_CHOICE
+                if [ "$INSTALL_CHOICE" = "n" ] || [ "$INSTALL_CHOICE" = "N" ]; then
+                    echo "取消安装！"
+                    break
+                fi
+                if [ -z "$INSTALL_CHOICE" ] || [ "$INSTALL_CHOICE" = "Y" ] || [ "$INSTALL_CHOICE" = "y" ]; then
+                    chmod +x "$BpName"
+                    sudo ./"$BpName"
+                    break
+                fi
+            else
+                echo "未检测到安装包，请先下载。"
                 break
             fi
-            if [ -z "$INSTALL_CHOICE" ] || [ "$INSTALL_CHOICE" = "Y" ] || [ "$INSTALL_CHOICE" = "y" ]; then
-                chmod +x "$BpName"
-                sudo ./"$BpName"
+            ;;
+        "准备破解补丁")
+            echo "$fav ...."
+            if [ -f "${BURP_DIR}${BURP}" ]; then
+                sudo cp -r CrackFiles/* "${BURP_DIR}${BURP}"
+                sudo chmod 644 "${BURP_DIR}${BURP}"ja-netfilter.jar
+                # sudo chmod 755 /opt/BurpSuitePro/plugins
+                # sudo chmod 755 /opt/BurpSuitePro/config
+                sudo rm -rf CrackFiles
+                ls -lah "${BURP_DIR}${BURP}"
+            else
+                echo "未检测到 ${BURP_DIR} 下的 BurpSuitePro 请先安装该程序。"
                 break
             fi
-        else
-            echo "未检测到安装包，请先下载。"
-            break
-        fi
-        ;;
-    "准备破解补丁")
-        echo "$fav ...."
-        if [ -f "${BURP_DIR}${BURP}" ]; then
-            sudo cp -r CrackFiles/* "${BURP_DIR}${BURP}"
-            sudo chmod 644 "${BURP_DIR}${BURP}"ja-netfilter.jar
-            # sudo chmod 755 /opt/BurpSuitePro/plugins
-            # sudo chmod 755 /opt/BurpSuitePro/config
-            sudo rm -rf CrackFiles
-            ls -lah "${BURP_DIR}${BURP}"
-        else
-            echo "未检测到 ${BURP_DIR} 下的 BurpSuitePro 请先安装该程序。"
-            break
-        fi
-        ;;
-    "运行注册机")
-        echo "$fav ...."
-        if [ -f "${BURP_DIR}${BURP}" ]; then
-            ("${BURP_DIR}${BURP}"jre/bin/java --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED -jar "${BURP_DIR}${BURP}${BURP_Loder}" -r) >/dev/null 2>&1 &
-            sleep 1s
-        else
-            echo "未检测到 ${BURP_DIR} 下的 BurpSuitePro 请先安装该程序。"
-            break
-        fi
-        ;;
-    "设置英文版")
-        echo "$fav ...."
-        if [ -f "${BURP_DIR}${BURP}" ]; then
-            sudo sed -i '10,25d' "${BURP_DIR}${BURP}"BurpSuitePro.vmoptions
-            sudo echo -e "\n--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED\n--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED\n-javaagent:${BURP_Loder}=loader\n-Xmx2048m" | sudo tee -a "${BURP_DIR}${BURP}"BurpSuitePro.vmoptions
-        else
-            echo "未检测到 ${BURP_DIR} 下的 BurpSuitePro 请先安装该程序。"
-            break
-        fi
-        ;;
-    "设置汉化版")
-        echo "$fav ...."
-        if [ -f "${BURP_DIR}${BURP}" ]; then
-            sudo sed -i '10，25d' "${BURP_DIR}${BURP}"BurpSuitePro.vmoptions
-            sudo echo -e "--add-opens=java.desktop/javax.swing=ALL-UNNAMED\n--add-opens=java.base/java.lang=ALL-UNNAMED\n--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED\n--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED\n--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED\n-javaagent:${BURP_Loder}=loader,han\n-Xmx2048m" | sudo tee -a "${BURP_DIR}${BURP}"/BurpSuitePro.vmoptions
-        else
-            echo "未检测到 ${BURP_DIR} 下的 BurpSuitePro 请先安装该程序。"
-            break
-        fi
-        ;;
-    "退出安装..")
-        echo "$fav ...."
-        exit
-        ;;
-    *) echo "无效选项 $REPLY" ;;
-    esac
+            ;;
+        "运行注册机")
+            echo "$fav ...."
+            if [ -f "${BURP_DIR}${BURP}" ]; then
+                ("${BURP_DIR}${BURP}"jre/bin/java --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED -jar "${BURP_DIR}${BURP}${BURP_Loder}" -r) >/dev/null 2>&1 &
+                sleep 1s
+            else
+                echo "未检测到 ${BURP_DIR} 下的 BurpSuitePro 请先安装该程序。"
+                break
+            fi
+            ;;
+        "设置英文版")
+            echo "$fav ...."
+            if [ -f "${BURP_DIR}${BURP}" ]; then
+                sudo sed -i '10,25d' "${BURP_DIR}${BURP}"BurpSuitePro.vmoptions
+                sudo echo -e "\n--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED\n--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED\n-javaagent:${BURP_Loder}=loader\n-Xmx2048m" | sudo tee -a "${BURP_DIR}${BURP}"BurpSuitePro.vmoptions
+            else
+                echo "未检测到 ${BURP_DIR} 下的 BurpSuitePro 请先安装该程序。"
+                break
+            fi
+            ;;
+        "设置汉化版")
+            echo "$fav ...."
+            if [ -f "${BURP_DIR}${BURP}" ]; then
+                sudo sed -i '10，25d' "${BURP_DIR}${BURP}"BurpSuitePro.vmoptions
+                sudo echo -e "--add-opens=java.desktop/javax.swing=ALL-UNNAMED\n--add-opens=java.base/java.lang=ALL-UNNAMED\n--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED\n--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED\n--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED\n-javaagent:${BURP_Loder}=loader,han\n-Xmx2048m" | sudo tee -a "${BURP_DIR}${BURP}"/BurpSuitePro.vmoptions
+            else
+                echo "未检测到 ${BURP_DIR} 下的 BurpSuitePro 请先安装该程序。"
+                break
+            fi
+            ;;
+        "退出安装..")
+            echo "$fav ...."
+            exit
+            ;;
+        *) echo "无效选项 $REPLY" ;;
+        esac
+    done
+    :
 done
 exit 0
